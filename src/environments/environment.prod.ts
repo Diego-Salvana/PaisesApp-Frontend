670878cfe724c8e7ba1936265b4bff72a5bae057
Diego Salvañá <diegofsalvana@gmail.com>
2023-07-11 18:49:13 -0300

    .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-   production: true,
-   baseCountriesUrl: 'https://restcountries.com/v3.1',
-   baseUsersUrl: 'https://paises-app-backend.vercel.app',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-   production: false,
-   baseCountriesUrl: 'https://restcountries.com/v3.1',
-   baseUsersUrl: 'https://paises-app-backend.vercel.app',
-};
